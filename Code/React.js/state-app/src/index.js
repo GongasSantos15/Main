e7b8faefa19a7c342ac6c3f148f971b142e9eb48
GongasSantos15 <goncalo.c.r.santos@protonmail.com>
2025-11-02 18:06:09 +0000

    Add States for Year and Manager

--- Code/React.js/state-app/src/index.js
+++ Code/React.js/state-app/src/index.js
@@ -4,16 +4,33 @@ import './index.css';
 
 // App Function
 function App() {
-  // Create state
+  // Create states
   const [status, setStatus] = useState("Open");
+  const [manager, setManager] = useState("Alex");
+  const [year, setYear] = useState(2050);
 
   return (
-    <div>
-      <h1>Status: {status}</h1>
-      <button onClick={() => setStatus("Open")}>Open</button>
-      <button onClick={() => setStatus("Back in 5")}>Break</button>
-      <button onClick={() => setStatus("Closed")}>Close</button>
-    </div>
+    <>
+      {/* Year */}
+      <div>
+        <h1>{year}</h1>
+        <button onClick={() => setYear(year + 1)}>New Year</button>
+      </div>
+
+      {/* Managers */}
+      <div>
+        <h1>Manager on Duty: {manager}</h1>
+        <button onClick={() => setManager("Rachel")}>New Manager</button>
+      </div>
+
+      {/* Status */}
+      <div>
+        <h1>Status: {status}</h1>
+        <button onClick={() => setStatus("Open")}>Open</button>
+        <button onClick={() => setStatus("Back in 5")}>Break</button>
+        <button onClick={() => setStatus("Closed")}>Close</button>
+      </div>
+    </>
   )
 }
 
