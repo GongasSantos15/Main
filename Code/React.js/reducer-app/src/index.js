2107913f98a8bde18f7d66593a441fe87609a104
GongasSantos15 <goncalo.c.r.santos@protonmail.com>
2025-11-02 19:13:49 +0000

    Implement a Checkbox using useReducer

--- Code/React.js/reducer-app/src/index.js
+++ Code/React.js/reducer-app/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import { React, useReducer } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Checkbox() {
+  const [checked, toggle] = useReducer(checked => !checked, false);
+
+  return (
+    <>
+      <input 
+        type="checkbox"
+        value={checked}
+        onChange={toggle}
+      />
+      {checked ? "checked" : "not checked"}
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Checkbox />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
