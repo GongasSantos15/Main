ec0de7c98ea5a401171ebf8cfe6ec23e7155e993
GongasSantos15 <goncalo.c.r.santos@protonmail.com>
2025-11-02 16:58:42 +0000

    Implement Conditional Rendering

--- Code/React.js/lake-skiresort/src/index.js
+++ Code/React.js/lake-skiresort/src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Function that shows the Lake
+function Lake({ name }) {
+  return (
+    <div>
+      <h1>Visit {name}!</h1>
+    </div>
+  );
+}
+
+// Function that shows the Ski Resort
+function SkiResort({ name }) {
+  return (
+    <div>
+      <h1>Visit {name}!</h1>
+    </div>
+  );
+}
+
+// App Function
+function App(props) {
+  return (
+    <div>
+      {props.season === "summer" ? (
+        <Lake name="Jenny Lake" />
+      ) : props.season === "winter" ? (
+        <SkiResort name="JHMR" />
+      ) : (
+        <h1>Come back in the winter or summer!</h1>
+      )}
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App season="fall" />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
