b7d4f31f0b89374ebc9f321b7cf6afcfc2d48a1d
GongasSantos15 <goncalo.c.r.santos@protonmail.com>
2025-11-02 12:33:00 +0000

    Create multiple elements

--- Code/React.js/first-element/src/index.js
+++ Code/React.js/first-element/src/index.js
@@ -4,4 +4,13 @@ import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(React.createElement("h1", { style: {color: "blue" } }, "Hello!"));
+
+// First Element Created
+// root.render(React.createElement("h1", { style: {color: "blue" } }, "Hello!"));
+
+// It is also possible to create a children element inside a parent one
+root.render(React.createElement(
+	"div", 
+	{ style: { color: "blue" } },
+	React.createElement("h1", null, "hi!")
+));
