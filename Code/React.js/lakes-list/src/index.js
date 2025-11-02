22db5b37ed44d0dbb2f7113177982b70d65fca88
GongasSantos15 <goncalo.c.r.santos@protonmail.com>
2025-11-02 16:16:06 +0000

    Create a List of Lakes

--- Code/React.js/lakes-list/src/index.js
+++ Code/React.js/lakes-list/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Lakes List
+const lakeList = [
+  "Echo Lake",
+  "Maud Lake",
+  "Cascade Lake"
+];
+
+// App Function
+function App({ lakes }) {
+  return (
+    <ul>
+      {lakes.map(lake => (
+        <li>{lake}</li>
+      ))}
+    </ul>
+  );
+}
+
+// Show the Content
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <App lakes={lakeList} />
+);
